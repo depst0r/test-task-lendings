ee4f8e4feddf551a913c81caaa84e14c128a66bb
depstor <depstor90@gmail.com>
2025-10-07 16:49:20 +0300

    task-1-v1.3

--- task_1/landing1/script.js
+++ task_1/landing1/script.js
@@ -15,9 +15,22 @@ window.addEventListener('DOMContentLoaded', () => {
             if (element.innerHTML.includes('Nano Slim')) {
                 element.innerHTML = element.innerHTML.replace(/Nano Slim/gi, 'Nutrilaben');
             }
-        });
+        })
     }
 
+    const toggleCountry = () => {
+        const elements = document.querySelectorAll('p, span, h1, h2, h3, h4, h5, h6, a, li')
+
+        elements.forEach(element => {
+            element.innerHTML = element.innerHTML
+                .replace(/Estados Unidos/gi, 'México')
+                .replace(/Israel/gi, 'México')
+                .replace(/España/gi, 'México')
+        })
+    }
+
+
+    toggleCountry()
     replaceProductName()
     changImgUrl()
 
