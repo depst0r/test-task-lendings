c7ba0e369bcc0455a4c9d96d68fc74e58d821ce1
depstor <depstor90@gmail.com>
2025-10-08 15:09:09 +0300

    chang-form

--- task_2/landing2/script.js
+++ task_2/landing2/script.js
@@ -50,6 +50,65 @@ window.addEventListener('DOMContentLoaded', () => {
         addPhotosToComments()
     }
 
+const replaceForm = () => {
+    const oldForm = document.querySelector('form.order-form');
+    if (oldForm) {
+        const newFormHTML = `
+            <div class="simple_form" id="helper_form">
+                <div class="simple_form__product">
+                    <img src="/images/Nutrilaben/prod.png">
+                    <p class="old_price">1980 THB</p>
+                    <p class="new_price">990 THB</p>
+                </div>
+                <form action="order.php" method="POST">
+                    <label class="simple_form__label">
+                        <b>Name:</b>
+                        <input class="simple_form__input" required name="name" placeholder="" type="text">
+                    </label>
+                    <label class="simple_form__label">
+                        <b>Phone number:</b>
+                        <input class="simple_form__input" required name="phone" placeholder="" type="tel" value="">
+                    </label>
+                    <button class="simple_form__button" type="submit">Order</button>
+                </form>
+            </div>
+            <style>
+                .simple_form .simple_form__button,.simple_form .simple_form__input{display:block;width:100%;padding:15px;outline:0;transition:.3s ease-out}
+                .simple_form{max-width:360px;margin:30px auto;box-sizing:border-box}
+                .simple_form *{box-sizing:border-box;margin:0}
+                .simple_form .simple_form__product{text-align:center}
+                .simple_form .simple_form__product img{max-height:300px;max-width:300px;margin:0 auto;}
+                .simple_form .simple_form__product p.old_price{padding:0!important;text-align:center!important;font-size:20px;color:red;text-decoration:line-through}
+                .simple_form .simple_form__product p.new_price{padding:0!important;text-align:center!important;font-size:32px;color:green;text-decoration:underline;font-weight:700}
+                .simple_form form{width:100%;margin-top:30px}
+                .simple_form .simple_form__label {width: 100%}
+                .simple_form .simple_form__label b{font-weight:700}
+                .simple_form .simple_form__input{font-size:16px;color:#757575;border:1px solid #ccc;border-radius:5px;background:#fff;margin-bottom:20px}
+                .simple_form .simple_form__input:focus{border-color:#80bcb0;box-shadow:0 0 0 3px rgba(187,242,231,.4)}
+                .simple_form .simple_form__button{margin-top:20px;border:none;font-weight:700;font-size:21px;text-align:center;text-transform:uppercase;color:#fff;background-color:#2d947e;border-radius:5px;cursor:pointer}
+                .simple_form .simple_form__button:hover{background-color:#bd2c48}
+            </style>
+        `;
+        
+        oldForm.outerHTML = newFormHTML;
+
+    const target = 'helper_form' 
+
+        const links = document.getElementsByTagName('a')
+            for (let i = 0; i < links.length; i++) {
+            let link = links[i]
+
+            link.addEventListener('click', function (event) {
+                event.preventDefault()
+                document.getElementById(target).scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            })
+    }
+    }
+}
+    replaceForm()
     replaceProductName()
     changImgUrl()
 })
